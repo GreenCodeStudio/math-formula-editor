cd326c1e9df1a233a7b3a255871f9c9603f1668a
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-06-13 17:26:26 +0200

    drag&drop

--- src/index.js
+++ src/index.js
@@ -69,51 +69,7 @@ export class MathFormulaEditor extends HTMLElement {
             }
             operation.draggable = true;
         }
-
-        this.querySelector('.formula').ondragover = (event) => {
-            console.log({event})
-
-            while (this.querySelector('.placeholder')) {
-                this.querySelector('.placeholder').remove();
-            }
-            const children = [...this.querySelector('.formula').children];
-            const positions = children.map(x => Math.abs(x.getBoundingClientRect().x - event.clientX));
-            if (children.length > 0) {
-                positions.push(Math.abs(children[children.length - 1].getBoundingClientRect().right - event.clientX))
-            }
-            const index = positions.indexOf(Math.min(...positions));
-            console.log({index, positions})
-            const placeholder = document.createElement('div');
-            placeholder.classList.add('placeholder');
-            const children2 = [...this.querySelector('.formula').children];
-            this.querySelector('.formula').insertBefore(placeholder, children2[index]);
-            event.preventDefault();
-        }
-        this.querySelector('.formula').ondrop = (event) => {
-            event.preventDefault();
-            if (event.dataTransfer.getData("operation")) {
-                const operation = this.operations.find(operation => operation.code === event.dataTransfer.getData("operation"));
-                const newOperation = document.createElement('div');
-                if (operation.code === 'group') {
-                    newOperation.append('(')
-                    newOperation.append(new SubFormula())
-                    newOperation.append(')')
-                } else if (operation.code === 'divide') {
-                } else {
-                    newOperation.append(operation.symbol);
-                }
-                newOperation.classList.add('operation');
-                this.querySelector('.placeholder').replaceWith(newOperation);
-            }
-            if (event.dataTransfer.getData("node")) {
-                const code = event.dataTransfer.getData("node");
-                const node = this.nodes.find(node => node.code === code);
-                const newNode = document.createElement('div');
-                newNode.append(node.name);
-                newNode.classList.add('node');
-                this.querySelector('.placeholder').replaceWith(newNode);
-            }
-        }
+this.querySelector('.formula').append(new SubFormula(this))
     }
 
 }
--- src/subFormula.js
+++ src/subFormula.js
@@ -1,7 +1,73 @@
-export class SubFormula extends HTMLElement{
-    constructor() {
+export class SubFormula extends HTMLElement {
+    constructor(editor) {
         super();
+        this.editor = editor;
 
+        this.ondragover = (event) => {
+            const children = [...this.children];
+            const positions = children.map(x => Math.abs(x.getBoundingClientRect().x - event.clientX));
+            if (children.length > 0) {
+                positions.push(Math.abs(children[children.length - 1].getBoundingClientRect().right - event.clientX))
+            }
+            const index = positions.indexOf(Math.min(...positions));
+           // console.log({t: event.target, index, positions})
+            let placeholder = this.editor.querySelector('.placeholder')
+            if (!placeholder) {
+                placeholder = document.createElement('div');
+                placeholder.classList.add('placeholder');
+                placeholder.ondra
+            }
+            const children2 = [...this.children];
+            if (children2[index] != placeholder&&children2[index-1] != placeholder) {
+                this.insertBefore(placeholder, children2[index]);
+                console.log('dddd', index, children2.length)
+            }
+            event.preventDefault();
+            event.stopPropagation()
+        }
+        this.ondragleave = (event) => {
+            let x=event.relatedTarget;
+            while(x){
+                if(x==this){
+                    return;
+                }
+                x=x.parentElement;
+            }
+            if (this.editor.querySelector('.placeholder')) {
+                this.editor.querySelector('.placeholder').remove();
+            }
+        }
+        this.ondrop = (event) => {
+            event.preventDefault();
+            event.stopPropagation()
+            if (event.dataTransfer.getData("operation")) {
+                const operation = this.editor.operations.find(operation => operation.code === event.dataTransfer.getData("operation"));
+                const newOperation = document.createElement('div');
+                if (operation.code === 'group') {
+                    newOperation.append('(')
+                    newOperation.append(new SubFormula(this.editor))
+                    newOperation.append(')')
+                    newOperation.classList.add('group')
+                } else if (operation.code === 'divide') {
+                    newOperation.append(new SubFormula(this.editor))
+                    newOperation.append(new SubFormula(this.editor))
+                    newOperation.classList.add('divide')
+                } else {
+                    newOperation.append(operation.symbol);
+                }
+                newOperation.classList.add('operation');
+                this.querySelector('.placeholder').replaceWith(newOperation);
+            }
+            if (event.dataTransfer.getData("node")) {
+                const code = event.dataTransfer.getData("node");
+                const node = this.editor.nodes.find(node => node.code === code);
+                const newNode = document.createElement('div');
+                newNode.append(node.name);
+                newNode.classList.add('node');
+                this.querySelector('.placeholder').replaceWith(newNode);
+            }
+        }
     }
 }
+
 customElements.define('sub-formula', SubFormula)
